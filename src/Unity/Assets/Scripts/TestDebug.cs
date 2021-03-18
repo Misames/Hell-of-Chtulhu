012c3698f7703d355c0b54effc6159a2478b2e41
François <fbouscaillou@myges.fr>
2021-03-18 23:10:05 +0100

    add Debug file

---
+++ src/Unity/Assets/Scripts/TestDebug.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class TestDebug : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Je suis sur la branch master");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
