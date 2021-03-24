022ed2acb83a58cb1dc4423e6f7931910ab82ec6
Francois Bouscaillou <fbouscaillou@myges.fr>
2021-03-24 23:19:42 +0100

    add old script Raphael

---
+++ src/Unity/Assets/Scripts/deadZone.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deadZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.name == "Enemy")
+        {
+            print("ca marche");
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
