548901305568a06b3e667eb4f4e237c6216a6ede
Michel MATHY <michel.mathy@yahoo.fr>
2021-04-03 19:17:09 +0200

    Ajout scripts ECS non modifié

---
+++ src/Unity/Assets/Scripts/ECS/TemplateUpdater.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Updater test avec déplacement vers la droite
+ * 
+ */
+public class TemplateUpdater : IUpdater
+{
+    // Déplacement vers la droite de tout les modules
+    public override void UpdateModules()
+    {
+        Accessor<TemplateModule> singletonAccessor = Accessor<TemplateModule>.Instance();
+        //Accessor<OtherTemplateModule> singletonOtherAccessor = Accessor<OtherTemplateModule>.Instance();
+
+        foreach (var dictEntry in singletonAccessor.GetAllModules())
+        {
+            TemplateModule mod= dictEntry.Value;
+            //OtherTemplateModule otherModule = singletonOtherAccessor.TryGetModule(dictEntry.Key);
+            
+            //Operations sur module
+            mod.objectTr.position = new Vector2(mod.objectTr.position.x+0.01f,mod.objectTr.position.y);
+
+            /*                       
+             module.data1 = something;
+             if(otherModule.data1==x)  do something;
+             
+             */
+
+        }
+
+    }
+}
