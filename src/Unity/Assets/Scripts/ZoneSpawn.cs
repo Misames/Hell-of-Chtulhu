5b5e0a928eb65bb8ec604c46d0731df34f5a036c
François Bouscaillou <fbouscaillou@myges.fr>
2021-06-20 20:37:49 +0200

    rename script zone

--- src/Unity/Assets/Scripts/ZoneSpawn.cs
+++ src/Unity/Assets/Scripts/ZoneSpawn.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class Test : MonoBehaviour
+public class ZoneSpawn : MonoBehaviour
 {
     public GameObject enemyManager;
     private void OnTriggerEnter(Collider other)
