ebebcdf15cce6dd75303b1e43ab5647d896c3957
Raphael <r.vejus@gmail.com>
2021-06-28 16:38:55 +0200

    cutscene modif

---
+++ src/Unity/Assets/Scripts/Cutscene/TriggerCutscene.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCutscene : MonoBehaviour
+{
+
+    public GameObject player;
+    public GameObject CutsceneCam;
+
+    private void Start()
+    {
+        CutsceneCam.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            this.gameObject.GetComponent<BoxCollider>().enabled = false;
+            CutsceneCam.SetActive(true);
+            player.SetActive(false);
+            StartCoroutine(FinCutscene());
+        }
+    }
+
+
+    IEnumerator FinCutscene()
+    {
+        yield return new WaitForSeconds(4);
+        player.SetActive(true);
+        CutsceneCam.SetActive(false);
+    }
+    
+}
